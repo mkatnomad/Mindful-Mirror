704c573ff1aec52c5f77d6c277c9a3ca7d6527a6
mkatnomad <mkatnomad@gmail.com>
2025-12-12 13:15:52 +0600

    Update geminiService.ts

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -48,7 +48,7 @@ const safetySettings = [
 const genAI = new GoogleGenerativeAI(getApiKey());
 
 // <--- ИСПРАВЛЕНИЕ: ИСПОЛЬЗУЕМ ТОЧНУЮ ВЕРСИЮ --->
-const MODEL_NAME = "models/gemma-3-4b-it"; 
+const MODEL_NAME = "gemma-3-12b"; 
 
 // --- ЧАТ ---
 export const sendMessageToGemini = async (
